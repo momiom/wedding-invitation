751c57b033c188123c711a906e8076993b693f03
momiom <kazuki.k.bl8+github.momio@gmail.com>
2022-06-14 22:52:59 +0900

    マップを追加

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,6 +15,17 @@ const Home = (): JSX.Element => {
       <section className="relative top-[-130px] w-full max-w-xl">
         <Message />
         <CelebrationDate />
+        <div className="p-8 map-wrapper">
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3241.693598557131!2d139.71087705174332!3d35.65991998010204!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60188bc5cdf417c7%3A0x8913224d4475c58d!2z44Ki44O844Oi44OL44O8IOOCouOCsOODrOOCouODvOODluODqy9IYXJtb25pZSBhZ3JlYWJsZQ!5e0!3m2!1sja!2sjp!4v1655051152267!5m2!1sja!2sjp"
+            width="600"
+            height="300"
+            className="border-0"
+            allowFullScreen={false}
+            loading="lazy"
+            referrerPolicy="no-referrer-when-downgrade"
+          ></iframe>
+        </div>
       </section>
       <section className="relative top-[-130px] w-full bg-primary-green border-t-2 border-off-white/50">
         <InvitationForm />
